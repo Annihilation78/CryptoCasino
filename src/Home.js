766a1171247592683b3e9cfbc3907e06b85f8046
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-25 19:45:27 +0100

    import

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './css/index.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Contact from './containers/Contact';
 
 function CasinoHomePage() {
