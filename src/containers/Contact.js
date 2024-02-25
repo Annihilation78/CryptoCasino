27e1e2fd8304154d2f72acc5e005256d14fb2d1f
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-25 19:04:27 +0100

    contacto

---
+++ src/containers/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Contacto</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="#" className="text-white">Inicio</a></li>
+            <li><a href="#" className="text-white">Juegos</a></li>
+            <li><a href="#" className="text-white">Promociones</a></li>
+            <li><a href="#" className="text-white">Soporte</a></li>
+          </ul>
+        </nav>
+      </header>
+      <section className="container mx-auto">
+          <h2 className="text-2xl font-bold mb-6">Información de Contacto</h2>
+          <div className="game-list grid grid-cols-2 gap-4">
+            <div className="game-item bg-white rounded-lg shadow-md p-4">
+              <p className="text-gray-700 mb-4">Puedes ponerte en contacto con nosotros a través de:</p>
+              <ul>
+                <li>Email: info@quantumbetbot.com</li>    
+                <li>Teléfono: +34 629812353</li>
+              </ul>
+              <button className="btn">Enviar correo</button>
+            </div>
+          </div>
+        </section>
+      <footer>
+        <p>© 2024 Casino. Todos los derechos reservados.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Contact;
