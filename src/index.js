33677bc368088a1d5c24dc5c20d7d71f206f10a3
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-25 19:49:28 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './Home';
+import Contact from './containers/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
+  
+);
+root.render(
+  <React.StrictMode>
+    <Contact />
+  </React.StrictMode>
+  
 );
