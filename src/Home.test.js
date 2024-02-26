c3712d5c5ad7699e09562917e42e6aca536b3681
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-26 20:57:29 +0100

    ELIMINADO

--- src/Home.test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CasinoHomePage from './Home'; // Cambia el nombre de la importación a CasinoHomePage
-
-ReactDOM.render(<CasinoHomePage />, document.getElementById('root'));
