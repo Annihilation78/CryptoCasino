8fb0ac12cdaf7a3f4c49833bd5670e441f1f0e68
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-26 20:35:57 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Home from './Home';
-import Contact from './containers/Contact';
-import { createBrowserRouter, RouterProvider, BrowserRouter, Router, Route, Routes, Link} from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./css/index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>
+    element: <div>Hello world!</div>,
   },
-  {
-    path: "home",
-    element: <Home/>
-  },
-  {
-    path: "contact",
-    element: <Contact/>
-  }
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-<React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-  
 );
