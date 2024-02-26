8134ada9327662b5158b5120e14d32d961d1811b
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-26 20:10:34 +0100

    router

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/index.css';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import BrowserRouter from 'react-router-dom';
 import Home from '../Home.js'
 
 function Contact() {
