74f1b79c60a9bdc8f66a2b898ec94a1aba0ab86a
Annihilation78 <giobarj78@gmail.com>
2024-02-27 21:26:48 +0100

    aa

--- CryptoCasino/src/App.jsx
+++ CryptoCasino/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './containers/Home';
-import Contact from './containers/Contact';
+import Home from './containers/home.jsx';
+import Contact from './containers/Contact.jsx';
 
 function App() {
   return (
