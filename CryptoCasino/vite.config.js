7d6608127e1d832d408d7d02afb3e62d8f43b1b0
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-02-27 20:40:37 +0100

    yaesta

--- CryptoCasino/vite.config.js
+++ CryptoCasino/vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import eslintPlugin from 'vite-plugin-eslint'
-import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin(), envCompatible()],
-  base: "/CryptoCasino",
-  css: {
-    // Example if you want to use preprocessors:
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "./src/styles/variables.scss";'
-      }
-    }
-  }
+  plugins: [react()],
+  base: '/CryptoCasino/',
 })
