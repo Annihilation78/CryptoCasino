0b1aa1fddc2119ef9bac6f827460122029137101
Annihilation78 <giobarj78@gmail.com>
2024-02-27 19:39:44 +0100

    aa

--- CryptoCasino/src/App.jsx
+++ CryptoCasino/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
-import Formulario from './containers/Contact';
+import Contact from './containers/Contact';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/Contact">
-          <Formulario />
+          <Contact />
         </Route>
         <Route path="/">
           <Home />
