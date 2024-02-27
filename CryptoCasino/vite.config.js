7fc314b3b4a6518b520fb9982845d2b5f5dafaee
yunqi-xu <55401502+yunqi-xu@users.noreply.github.com>
2024-02-27 20:33:43 +0100

    fdgfdgdfgdgf

--- CryptoCasino/vite.config.js
+++ CryptoCasino/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import eslintPlugin from 'vite-plugin-eslint'
+import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/CryptoCasino/',
+  plugins: [react(), eslintPlugin(), envCompatible()],
+  base: "annihilation78.github.io/CryptoCasino/",
+  css: {
+    // Example if you want to use preprocessors:
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/variables.scss";'
+      }
+    }
+  }
 })
