eb8152884e14167f21d5b5bfd3500d8d8e8c2636
Annihilation78 <giobarj78@gmail.com>
2024-02-28 19:14:18 +0100

    aaaa

--- CryptoCasino/vite.config.js
+++ CryptoCasino/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? 'https://annihilation78.github.io/CryptoCasino/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/CryptoCasino/' : '/',
 })
