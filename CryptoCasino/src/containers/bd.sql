df106cc548de2f133479e5110cda2710bb231c77
Annihilation78 <giobarj78@gmail.com>
2024-02-29 20:28:04 +0100

    aaa

--- CryptoCasino/src/containers/bd.sql
+++ CryptoCasino/src/containers/bd.sql
@@ -3,8 +3,8 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL, -- consider using a hashed password for security
-  wallet DECIMAL(10, 2) NOT NULL DEFAULT 0.00 -- wallet for the user
+  password VARCHAR(255) NOT NULL, 
+  wallet DECIMAL(10, 2) NOT NULL DEFAULT 0.00 
 );
 
 
