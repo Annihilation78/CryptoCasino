3c1df35a1de6552b5f00a80b4f9c4c8bd3c03f2b
Annihilation78 <giobarj78@gmail.com>
2024-03-01 21:35:50 +0100

    aaa

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,41 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
-import '../css/contacto.css';
-
+import "../css/contacto.css";
 
 function Contact() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
-    alert('¡Formulario enviado con éxito!');
+    alert("¡Formulario enviado con éxito!");
   };
 
   return (
     <div className="app">
       <header className="header">
-      <h1>Contacto</h1>
+        <h1>Contacto</h1>
         <nav className="main-nav">
           <ul className="menu">
-            <li><Link to="/">Inicio</Link></li>
-            <li><Link to="/juegos">Juegos</Link></li>
-            <li><Link to="/promociones">Promociones</Link></li>
-            <li><Link to="/soporte">Soporte</Link></li>
-            <li><Link to="/contact">Contacto</Link></li>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/juegos">Juegos</Link>
+            </li>
+            <li>
+              <Link to="/promociones">Promociones</Link>
+            </li>
+            <li>
+              <Link to="/soporte">Soporte</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contacto</Link>
+            </li>
           </ul>
         </nav>
       </header>
       <section className="contact-section">
         <div className="container mx-auto">
           <h2>Contacta con nosotros</h2>
-          <p>¿Tienes preguntas, sugerencias o quieres trabajar con nosotros? ¡No dudes en contactarnos!</p>
+          <p>
+            ¿Tienes preguntas, sugerencias o quieres trabajar con nosotros? ¡No
+            dudes en contactarnos!
+          </p>
 
           <div className="contact-info">
             <ul>
-              <li><i className="fas fa-phone"></i> +34 629812353</li>
-              <li><i className="fas fa-envelope"></i> info@quantumbetbot.com</li>
+              <li>
+                <i className="fas fa-phone"></i> +34 629812353
+              </li>
+              <li>
+                <i className="fas fa-envelope"></i> info@quantumbetbot.com
+              </li>
             </ul>
           </div>
 
@@ -45,7 +65,7 @@ function Contact() {
                 type="text"
                 name="nombre"
                 placeholder="Tu nombre"
-                {...register('nombre', { required: "Este campo es requerido" })}
+                {...register("nombre", { required: "Este campo es requerido" })}
               />
               {errors.nombre && <p>{errors.nombre.message}</p>}
 
@@ -53,12 +73,13 @@ function Contact() {
                 type="email"
                 name="email"
                 placeholder="Tu email"
-                {...register('email', {
+                {...register("email", {
                   required: "Este campo es requerido",
                   pattern: {
                     value: /^\S+@\S+$/i,
-                    message: "Por favor, ingresa una dirección de correo válida"
-                  }
+                    message:
+                      "Por favor, ingresa una dirección de correo válida",
+                  },
                 })}
               />
               {errors.email && <p>{errors.email.message}</p>}
@@ -66,11 +87,15 @@ function Contact() {
               <textarea
                 name="mensaje"
                 placeholder="Tu mensaje"
-                {...register('mensaje', { required: "Este campo es requerido" })}
+                {...register("mensaje", {
+                  required: "Este campo es requerido",
+                })}
               />
               {errors.mensaje && <p>{errors.mensaje.message}</p>}
 
-              <button type="submit" className="btn">Enviar mensaje</button>
+              <button type="submit" className="btn">
+                Enviar mensaje
+              </button>
             </form>
           </div>
         </div>
@@ -78,10 +103,18 @@ function Contact() {
 
       <footer>
         <p>© 2024 Casino. Todos los derechos reservados.</p>
+        <p>
+          <a href="http://jigsaw.w3.org/css-validator/check/referer">
+            <img
+              style="border:0;width:88px;height:31px"
+              src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+              alt="¡CSS Válido!"
+            />
+          </a>
+        </p>
       </footer>
     </div>
   );
 }
 
 export default Contact;
-
