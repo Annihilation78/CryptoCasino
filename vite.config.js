35c8c5e1f0b48fac8e91a5054346a5b01ea3d878
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-01 20:05:37 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/CryptoCasino/' : '/',
+  base: '/CryptoCasino/',
+  plugins: [react()]
 })
