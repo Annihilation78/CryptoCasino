2fc5b7c41d11a4de47422661f1cc7faa5cda8a33
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-01 09:34:24 +0100

    browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as BrowserRouter, Router, Routes, Route } from 'react-router-dom';
 import Home from './containers/Home.jsx';
 import Contact from './containers/Contact.jsx';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/CryptoCasino/'}>
+      <Router>
+        <Routes>
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </BrowserRouter>
   );
 }
 
