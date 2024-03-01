9071addd66eba2b29d57a9bddf61499fb6e8e9ac
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-01 19:52:12 +0100

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/CryptoCasino/' : '/',
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [
+      react(),
+      viteCompression()
+    ],
+    assetsInclude: ['**/*.jpg', '**/*.png', '**/*.svg', '**/*.gif'],
+    base: './'
+  }
+});
