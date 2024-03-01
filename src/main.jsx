1f9eec8715159d181d79c777b42be714546031c7
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-01 09:38:26 +0100

    router

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ const root =  ReactDOM.createRoot(document.getElementById('root'))
 root.render
 (
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={"/CryptoCasino/"}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
