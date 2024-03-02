ddd85f2bda88552691c2194dd916fff43623951a
yunqi-xu <55401502+yunqi-xu@users.noreply.github.com>
2024-03-02 20:24:50 +0100

    dfsdfsdf

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -76,6 +76,13 @@ function Home() {
       </div>
       <footer>
         <p>© 2024 Casino. Todos los derechos reservados.</p>
+        <p>
+          <a href="https://jigsaw.w3.org/css-validator/check/referer">
+            <img style={{ border: '0', width: '88px', height: '31px' }}
+              src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+              alt="¡CSS Válido!" />
+          </a>
+        </p>
       </footer>
     </div>
   );
