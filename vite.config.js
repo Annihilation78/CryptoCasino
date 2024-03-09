254daab6c2345125c7b16bc523b9f055ba4194ce
Annihilation78 <giobarj78@gmail.com>
2024-03-09 18:48:16 +0100

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+    test: {
+      // <--- Add this object
+      globals: true,
+      environment: "jsdom",
+    },
   base: process.env.NODE_ENV === 'production' ? '/CryptoCasino/' : '/',
 })
