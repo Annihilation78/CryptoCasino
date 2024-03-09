f38348241726c07573cb053c8e52da960cefa81c
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-09 19:08:54 +0100

    home

---
+++ src/containers/Home.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+
+import Home from './Home';
+
+describe('Home', () => {
+  it('renders headline', () => {
+    render(<Home title="Bienvenido al Casino" />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
