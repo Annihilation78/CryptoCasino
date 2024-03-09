90727ac3baf9ffd7e773489b727c38cfa381f87c
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-09 18:20:47 +0100

    revert

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-      "^.+\\.jsx?$": "babel-jest",
-      "^.+\\.css$": "jest-transform-css"
-    },
-    moduleNameMapper: {
-      "\\.(css|less)$": "identity-obj-proxy"
-    }
-  };
