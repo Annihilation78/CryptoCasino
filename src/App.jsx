a3deeee4a857204f257d8a48bf1e3fc829587fa0
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-10 17:03:01 +0100

    import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './containers/Home.jsx';
 import Contact from './containers/Contact.jsx';
+import Navigation from './containers/Navigation.jsx';
+
 
 function App() {
   return (
