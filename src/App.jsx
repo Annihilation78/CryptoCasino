7fed81b7a3de98ef7206b33991bbe83ac1919515
Annihilation78 <giobarj78@gmail.com>
2024-03-11 19:19:20 +0100

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './containers/Home.jsx';
 import Contact from './containers/Contact.jsx';
 import Navigation from './containers/Navigation.jsx';
-<<<<<<< HEAD
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 
-=======
 import Login from './containers/Login/Login.jsx';
 import Register from './containers/Login/Register.jsx';
->>>>>>> 1ddd4cec932a426b0ba57d9781eef76874a12927
 
 
 function App() {
