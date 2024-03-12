d066ccbdcd8ee8f36ae0351c3768fbd84879a554
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-12 19:23:29 +0100

    register

--- src/containers/Login/Register.jsx
+++ src/containers/Login/Register.jsx
@@ -4,6 +4,14 @@ import { Link } from "react-router-dom";
 const Register = () => {
 
   return (
+        <div className="app">
+        <header className="header">
+          <h1>
+          <img src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/Logo.png" alt="Logo"/>
+            Quantum Bet Bot
+          </h1>
+          <Navigation />
+        </header>
         <div class="login-container">
         <h2>Crea tu cuenta</h2>
         <form action="">
@@ -21,10 +29,21 @@ const Register = () => {
           </div>
           <button type="submit" class="login-btn">Crear cuenta</button>
           <div>
-            <li><Link to="/login">¿Ya tienes cuenta?</Link></li>
+            <li><Link to="/">¿Ya tienes cuenta?</Link></li>
           </div>
         </form>
         </div>
+        <footer> 
+          <p>© 2024 Casino. Todos los derechos reservados.</p>
+          <p>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer">
+              <img style={{ border: '0', width: '88px', height: '31px' }}
+                src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                alt="¡CSS Válido!" />
+            </a>
+          </p>
+        </footer>
+      </div>
   );
 }
 
