a07336c1c16cf68dc7808dcbaf2daa4c68b267a4
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-26 11:55:45 +0100

    nuevo

--- src/containers/Login/Register.jsx
+++ src/containers/Login/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import Navigation from '../Navigation.jsx';
 
 function Register() {
