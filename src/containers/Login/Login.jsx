1eb101add371d1d3da8f8b5e0535cec94cd4bde8
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-03-26 11:54:47 +0100

    cambios

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+function Login() {
 
   const {
     register,
