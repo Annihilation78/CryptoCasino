9a2a91eff5cbb7d1ce5eef262b58bb31c679b21b
Annihilation78 <giobarj78@gmail.com>
2024-05-01 13:01:48 +0200

    aa

--- src/containers/Games.jsx
+++ src/containers/Games.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { motion } from 'framer-motion'; // Importamos Framer Motion
 import '../css/Games.css'; // Importa tu archivo CSS
+import { Link } from 'react-router-dom';
 
 function MenuJuegos() {
     return (
         <div className="menu">
             <motion.div whileHover={{ scale: 1.1 }}>
-                <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/jack.jpg" alt="juego 1" />
-                <button className="boton-juego" onClick={() => console.log('Juego 1 seleccionado')}>
-                    Juego 1
-                </button>
+                <Link to="/BlackJackGame">
+                    <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/jack.jpg" alt="juego 1" />
+                    <div className="descripcion-juego">BlackJack</div>
+                </Link>
             </motion.div>
             <motion.div whileHover={{ scale: 1.1 }}>
-                <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/Roulette.png" alt="juego 2" />
-                <button className="boton-juego" onClick={() => console.log('Juego 2 seleccionado')}>
-                    Juego 2
-                </button>
+                <Link to="/Roulette">
+                    <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/Roulette.png" alt="juego 2" />
+                    <div className="descripcion-juego">Roulette</div>
+                </Link>
             </motion.div>
         </div>
     );
