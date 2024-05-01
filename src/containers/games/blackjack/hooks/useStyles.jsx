66b2309154dc07273ab8c5b37d8bdf5c7c0029de
Annihilation78 <giobarj78@gmail.com>
2024-05-01 16:52:06 +0200

    aa

--- src/containers/games/blackjack/hooks/useStyles.jsx
+++ src/containers/games/blackjack/hooks/useStyles.jsx
@@ -1,33 +1,35 @@
-import { makeStyles } from "@material-ui/styles";
+import { styled } from '@mui/system';
 
-export const useStyles = makeStyles({
-  playArea: {
-    margin: "10px 25px"
-  },
-  coinArea: {
-    height: "100px",
-    padding: "10px",
-    color: "#fff"
-  },
-  messageArea: {
-    margin: "10px"
-  },
-  message: {
-    fontSize: "22px",
-    color: "white"
-  },
-  winOrLoseContainer: {
-    height: "50px"
-    // margin: "-50px auto 0px"
-  },
-  winOrLose: {
-    border: "1px solid black",
-    backgroundColor: "grey",
-    height: "40px",
-    fontSize: "1.3em",
-    fontWeight: "bold",
-    color: "white",
-    lineHeight: "40px",
-    opacity: 0.8
-  }
+export const PlayArea = styled('div')({
+  margin: '10px 25px',
+});
+
+export const CoinArea = styled('div')({
+  height: '100px',
+  padding: '10px',
+  color: '#fff',
+});
+
+export const MessageArea = styled('div')({
+  margin: '10px',
+});
+
+export const Message = styled('div')({
+  fontSize: '22px',
+  color: 'white',
+});
+
+export const WinOrLoseContainer = styled('div')({
+  height: '50px',
+});
+
+export const WinOrLose = styled('div')({
+  border: '1px solid black',
+  backgroundColor: 'grey',
+  height: '40px',
+  fontSize: '1.3em',
+  fontWeight: 'bold',
+  color: 'white',
+  lineHeight: '40px',
+  opacity: 0.8,
 });
