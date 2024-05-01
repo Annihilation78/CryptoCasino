7029da14de19b6e2ecd79ff57ed31e222225355e
yunqi-xu <55401502+yunqi-xu@users.noreply.github.com>
2024-05-01 16:06:58 +0200

    asdasd

--- src/containers/Games.jsx
+++ src/containers/Games.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion'; // Importamos Framer Motion
 import '../css/Games.css'; // Importa tu archivo CSS
 import { Link } from 'react-router-dom';
 import Navigation from './Navigation.jsx'; 
-import BlackJack from "./BlackJack";
 
 function MenuJuegos() {
     return (
@@ -18,7 +17,7 @@ function MenuJuegos() {
 
             <div className="menu">
                 <motion.div whileHover={{ scale: 1.1 }}>
-                    <Link to="/BlackJack">
+                    <Link to="/Roulette">
                         <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/jack.jpg" alt="juego 1" />
                         <div className="descripcion-juego">BlackJack</div>
                     </Link>
