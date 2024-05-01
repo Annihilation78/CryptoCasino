a3d7d31036d854d6a794f9fa0bfb73fbd1db11a5
Annihilation78 <giobarj78@gmail.com>
2024-05-01 13:33:43 +0200

    aaa

---
+++ src/containers/Promotions.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { motion } from 'framer-motion'; // Importamos Framer Motion
+import '../css/Games.css'; // Importa tu archivo CSS
+import { Link } from 'react-router-dom';
+
+function MenuPromociones() {
+    return (
+        <div className="menu">
+            <motion.div whileHover={{ scale: 1.1 }}>
+                    <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/jack.jpg" alt="juego 1" />    
+            </motion.div>
+            <motion.div whileHover={{ scale: 1.1 }}>
+                    <img className="imagen-juego" src="https://github.com/Annihilation78/CryptoCasino/raw/main/src/assets/promocion" alt="juego 2" />
+            </motion.div>
+        </div>
+    );
+}
+
+export default MenuPromociones;
