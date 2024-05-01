4c2f64452ede5049489d6c2042fac8015234da75
yunqi-xu <55401502+yunqi-xu@users.noreply.github.com>
2024-05-01 14:45:04 +0200

    asdasdasd

--- src/containers/games/blackjack/Deck.jsx
+++ src/containers/games/blackjack/Deck.jsx
@@ -2,33 +2,47 @@
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-const images = values.reduce((acc, value) => {
-  suits.forEach(suit => {
-    // Importa y asigna cada imagen usando import dinámico, que retorna una promesa
-    acc[`${value}_of_${suit}`] = import(`/assets/${value}_of_${suit}.png`);
-  });
-  return acc;
-}, {});
+// Preparar para cargar todas las imágenes.
+const loadImages = async () => {
+    const images = {};
+    for (let suit of suits) {
+        for (let value of values) {
+            const image = await import(`/assets/${value}_of_${suit}.png`);
+            images[`${value}_of_${suit}`] = image.default;
+        }
+    }
+    return images;
+};
+
+let loadedImages;
+
+// Cargar imágenes cuando el módulo es evaluado.
+const imagesPromise = loadImages().then(images => {
+    loadedImages = images;
+});
 
 export const initializeDeck = async () => {
-  let deck = [];
-  for (let suit of suits) {
-    for (let value of values) {
-      deck.push({
-        suit,
-        value,
-        imageUrl: await images[`${value}_of_${suit}`] // Espera a que la promesa se resuelva
-      });
+    // Asegúrate de que las imágenes estén cargadas antes de inicializar el mazo.
+    await imagesPromise;
+
+    let deck = [];
+    for (let suit of suits) {
+        for (let value of values) {
+            deck.push({
+                suit,
+                value,
+                imageUrl: loadedImages[`${value}_of_${suit}`]
+            });
+        }
     }
-  }
-  return deck;
+    return deck;
 };
 
 export const shuffleDeck = (deck) => {
     let deckCopy = [...deck];
     for (let i = deckCopy.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [deckCopy[i], deckCopy[j]] = [deckCopy[j], deckCopy[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [deckCopy[i], deckCopy[j]] = [deckCopy[j], deckCopy[i]];
     }
     return deckCopy;
-  };
+};
