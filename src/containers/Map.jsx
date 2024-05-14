ff4d83225faf6fd8a8e7a5ce286dc9487ecf7d1f
Annihilation78 <giobarj78@gmail.com>
2024-05-14 20:38:56 +0200

    aa

---
+++ src/containers/Map.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+
+function MapComponent() {
+  const position = [37.7749, -122.4194];
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <MapContainer center={position} zoom={13} style={{ height: "300px", width: "500px" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default MapComponent;
+
+
+
--- src/containers/Support.jsx
+++ src/containers/Support.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header.jsx'; 
 import Footer from './Footer.jsx';
 import '../css/Support.css';
+import MapComponent from './Map.jsx';
 
 function Support() {
     return (
@@ -10,6 +11,7 @@ function Support() {
                 <Header title="Soporte"/>
             </div>
             <h1 className="support-title">Página de Soporte</h1>
+            <MapComponent />
             <p className="support-text">Bienvenido a nuestra página de soporte. Aquí puedes encontrar ayuda y recursos relacionados con nuestros productos y servicios.</p>
             {/* Puedes agregar más contenido aquí */}
             <div className="header-footer-container footer">
