1b7290690d90527fab4f3338d73c46730051fe13
Annihilation78 <giobarj78@gmail.com>
2024-05-14 17:26:55 +0200

    aa

---
+++ src/containers/Support.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header.jsx'; 
+import Footer from './Footer.jsx';
+
+function PaginaSoporte() {
+    return (
+        <div>
+            <h1>Página de Soporte</h1>
+            <p>Bienvenido a nuestra página de soporte. Aquí puedes encontrar ayuda y recursos relacionados con nuestros productos y servicios.</p>
+            {/* Puedes agregar más contenido aquí */}
+        </div>
+    );
+    <Footer />
+}
+
+export default PaginaSoporte;
