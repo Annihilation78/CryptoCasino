ad1935a987f77353e868adb542fb48ea85838184
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-05-15 21:13:49 +0200

    protected

--- src/containers/ProtectedRoute.jsx
+++ src/containers/ProtectedRoute.jsx
@@ -1,8 +1,12 @@
 import { Navigate } from "react-router-dom";
-import {Auth} from "./login/Auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export const ProtectedRoute = ({ children }) => {
-  const { user } = Auth();
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
   if (!user) {
     // user is not authenticated
     return <Navigate to="/login" />;
