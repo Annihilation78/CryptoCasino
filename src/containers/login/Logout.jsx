47a7d2c5fd7ba772e0e4e4d6ff89fdc33a7359a6
Dideiras <160527270+Dideiras@users.noreply.github.com>
2024-05-27 22:24:54 +0200

    logout

--- src/containers/login/Logout.jsx
+++ src/containers/login/Logout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './Auth.jsx'; // Importa la función de cierre de sesión de Firebase
 
