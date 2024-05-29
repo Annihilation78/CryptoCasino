3c4d7d5b6027ef1712af679664f0bfa3751ca2a8
yunqi-xu <55401502+yunqi-xu@users.noreply.github.com>
2024-05-29 23:23:31 +0200

    asd

--- src/containers/login/Register.jsx
+++ src/containers/login/Register.jsx
@@ -13,10 +13,10 @@ import { signInWithEmailAndPassword, updateProfile } from 'firebase/auth';
 
 export const getBalance = async (userId) => {
   try {
-    const userDoc = await getDoc(doc(db, 'users', userId));
+    const userDoc = await getDoc(doc(db, 'users', user.uid), { balance: newBalance });
     if (userDoc.exists()) {
       const userData = userDoc.data();
-      const balance = userData.balance;
+      const balance = newBalance;
       return balance;
     } else {
       console.error("No such document!");
